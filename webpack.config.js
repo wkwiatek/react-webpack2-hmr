28388ffe21632a71a537803391ca5d80f1f1132b
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2017-01-05 14:05:01 +0100

    Packages updated

--- webpack.config.js
+++ webpack.config.js
@@ -16,21 +16,19 @@ const config = {
     filename: '[name].js'
   },
   resolve: {
-    extensions: ['', '.ts', '.es6', '.js', '.json']
+    extensions: ['.ts', '.js', '.json']
   },
   module: {
-    // preLoaders: [
-    //   { test: /\.js$/, exclude: /node_modules/, loader: 'eslint' }
-    // ],
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-      { test: /\.json$/, loader: 'json' },
-      { test: /\.html/, loader: 'html' },
-      { test: /\.styl$/, loader: 'css!stylus' },
-      { test: /\.css$/, loader: 'style!css' },
-      { test: /\.(gif|png|jpe?g)$/i, loader: 'file?name=dist/images/[name].[ext]' },
-      { test: /\.woff2?$/, loader: 'url?name=dist/fonts/[name].[ext]&limit=10000&mimetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)$/, loader: 'file?name=dist/fonts/[name].[ext]' }
+    rules: [
+      // { enforce: 'pre', test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.html/, loader: 'html-loader' },
+      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.(gif|png|jpe?g)$/i, loader: 'file-loader?name=dist/images/[name].[ext]' },
+      { test: /\.woff2?$/, loader: 'url-loader?name=dist/fonts/[name].[ext]&limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)$/, loader: 'file-loader?name=dist/fonts/[name].[ext]' }
     ]
   }
 };
